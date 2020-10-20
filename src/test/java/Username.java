631b265cb6851965df7a7cfc9fba0323bf638b6c
AleksanderVEriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2020-10-20 21:02:21 +0200

    Junit dep

--- src/test/java/Username.java
+++ src/test/java/Username.java
@@ -1,2 +1,14 @@
+import KontoInformasjon.Konto;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class Username {
+    Konto user = new Konto("Test", "Test");
+    @Test
+    public void testUsername(){
+
+        Assert.assertFalse(user.getNavn(), );
+
+
+    }
 }
