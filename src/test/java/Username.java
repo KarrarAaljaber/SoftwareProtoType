5c8fc624a734b6634f797af08c45a833e94ab2c7
AleksanderVEriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2020-10-21 21:06:01 +0200

    Json konto test (navn og passord)

--- src/test/java/Username.java
+++ src/test/java/Username.java
@@ -1,27 +1,63 @@
 import KontoInformasjon.Konto;
-import ProtoType.LaunchProtoType;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import Repo.JSONRepo;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.File;
+import java.util.ArrayList;
 
 public class Username {
-    Konto user = new Konto("Test", "Test");
+    Konto user = new Konto();
+    ArrayList<Konto> KontoList = new ArrayList<>();
+    JSONRepo konto = new JSONRepo();
+    @ParameterizedTest
+    @ValueSource(strings = {"Yaqub","bag","Test","Gay","SSs"})
+    public void testUsername(String input) {
+        KontoList = konto.LoadFile2("Konto.json");
+        boolean t;
+        for (Konto value : KontoList) {
+            user.setNavn(input);
+            t = value.getNavn().equals(input);
+            //System.out.println(value.getNavn() + " and " + input + " is " + t);
+            if(t){
 
-    @Test
-    public void testUsername() {
-        TextField name = new TextField();
-        name.setText("Test");
-        Assert.assertEquals(user.getNavn(), name.getText());
+                Assert.assertEquals(value.getNavn(), user.getNavn());
+                System.out.println("Username " + user.getNavn() + " exists");
+                break;
+            }
+            else{
+                Assert.assertFalse(false);
+                //System.out.println("Username " + user.getNavn() + " does not exist");
+                }
+            }
+        //System.out.println(input);
+
+        }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123","bag","Test","Gay","SSs"})
+    public void testPassword(String pass){
+        KontoList = konto.LoadFile2("Konto.json");
+        boolean t;
+        for (Konto value : KontoList) {
+            user.setPassord(pass);
+            t = value.getPassord().equals(pass);
+            //System.out.println(value.getNavn() + " and " + input + " is " + t);
+            if(t){
+
+                Assert.assertEquals(value.getPassord(), user.getPassord());
+                System.out.println("Password " + user.getPassord() + " exists");
+                break;
+            }
+            else{
+                Assert.assertFalse(false);
+                //System.out.println("Username " + user.getNavn() + " does not exist");
+            }
+        }
+        //System.out.println(input);
+
     }
-    @Test
-    public void testPassword(){
-        PasswordField pass = new PasswordField();
-        pass.setText("Test");
-        Assert.assertEquals(user.getNavn(), pass.getText());
     }
-}
+
