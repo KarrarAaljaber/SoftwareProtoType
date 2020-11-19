8ac2462a82a355ab960cdbf8f9bfa26f54cb6f66
AleksanderVEriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2020-11-19 17:38:20 +0100

    Register test + view change

---
+++ src/test/java/checkBestilling.java
@@ -0,0 +1,16 @@
+import Parkeringsplass.Bestilling;
+import ProtoType.BestillingView;
+
+import java.util.ArrayList;
+
+public class checkBestilling {
+
+    ArrayList<Bestilling> bestillinger = new ArrayList<>();
+    //BestillingView test = new BestillingView();
+
+
+
+
+
+
+}
